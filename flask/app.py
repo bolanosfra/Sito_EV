107b98ae5bb8fb79031563551d137213a795c8a1
Saccullo-p <saccullo.pietro@iisgalvanimi.edu.it>
2022-05-14 13:29:03 +0000

    sito_progetto

--- flask/app.py
+++ flask/app.py
@@ -6,13 +6,12 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://sac:Camera1@cluster0-shard-00-00.x1eis.mongodb.net:27017,cluster0-shard-00-01.x1eis.mongodb.net:27017,cluster0-shard-00-02.x1eis.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-20bimn-shard-0&authSource=admin&retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb://sac:Camera1@cluster0-shard-00-00.x1eis.mongodb.net:27017,cluster0-shard-00-01.x1eis.mongodb.net:27017,cluster0-shard-00-02.x1eis.mongodb.net:27017/Progetto_Sito?ssl=true&replicaSet=atlas-20bimn-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
 
 # Per rispondere alle chiamate cross origin
 CORS(app)
-api = API(app)
 
 @app.route("/dati")
 def get():
