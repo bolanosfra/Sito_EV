f99e7669f98fa591acd918d11dfc0fc9572f0c2b
Saccullo-p <saccullo.pietro@iisgalvanimi.edu.it>
2022-05-18 09:46:50 +0000

    18.05.22

--- ev_stations/src/app/app.module.ts
+++ ev_stations/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {MostraComponent} from './mostra/mostra.component';
+import { Parte1Component } from './parte1/parte1.component';
+import { Parte2Component } from './parte2/parte2.component';
+import { Parte3Component } from './parte3/parte3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MostraComponent,
+    Parte1Component,
+    Parte2Component,
+    Parte3Component,
   ],
   imports: [
     BrowserModule,
